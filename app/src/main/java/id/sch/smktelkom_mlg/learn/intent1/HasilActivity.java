e43cb9a16258b686caeffe96eae2abc38e02ff61
yogasulton <yogamuslim21@gmail.com>
2016-11-01 08:59:42 +0700

    onBackPressed part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.learn.intent1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 
 public class HasilActivity extends AppCompatActivity {
@@ -18,5 +19,15 @@ public class HasilActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
